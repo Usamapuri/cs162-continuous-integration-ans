ca128bcc459efd3c955ca6777951c15027f94112
Usama Puri <55175411+Usamapuri@users.noreply.github.com>
2021-04-06 21:57:33 -0700

    Update test.py

--- web/test.py
+++ web/test.py
@@ -2,32 +2,32 @@ import unittest
 import requests
 from sqlalchemy import create_engine
 
-class DockerTestCase(unittest.TestCase):
+class TestCases(unittest.TestCase):
 
     def test_endpoint_success_case(self):
-        request = requests.post('http://127.0.0.1:5000/add', data={'expression':'100+100'})
+        request = requests.post('http://127.0.0.1:5000/add', data={'expression':'1 +1 '})
         self.assertEqual(r.status_code, 200)
 
     def test_endpoint_error_case(self):
-        request = requests.post('http://127.0.0.1:5000/add', data={'expression':'100+'})
+        request = requests.post('http://127.0.0.1:5000/add', data={'expression':'lolol'})
         self.assertNotEqual(request.status_code, 200)
 
     def test_db_success(self):
-        request = requests.post('http://127.0.0.1:5000/add', data={'expression':'100+100'})
+        request = requests.post('http://127.0.0.1:5000/add', data={'expression':'1 +1'})
         engine = create_engine('postgresql://cs162_user:cs162_password@127.0.0.1:5432/cs162', echo = True)
 
         with engine.connect() as con:
-            query_string = con.execute("SELECT * FROM Expression WHERE text = '100+100'")
+            query_string = con.execute("SELECT * FROM Expression WHERE text = '1 + 1")
             rows = query_string.fetchall()
 
         self.assertNotEqual(len(rows), 0)
 
     def test_error_db(self):
-        request = requests.post('http://127.0.0.1:5000/add', data={'expression':'100+'})
+        request = requests.post('http://127.0.0.1:5000/add', data={'expression':'lolololo'})
         engine = create_engine('postgresql://cs162_user:cs162_password@127.0.0.1:5432/cs162', echo = True)
 
         with engine.connect() as con:
-            query_string = con.execute("SELECT * FROM Expression WHERE text = '100+'")
+            query_string = con.execute("SELECT * FROM Expression WHERE text = 'lololo'")
             rows = query_string.fetchall()
 
         self.assertEqual(len(rows), 0)
